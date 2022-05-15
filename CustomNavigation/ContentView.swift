efca60c09c804853bb64eb23fdbad8c2484fcfc4
Jonni <enterthesource@me.com>
2022-05-15 22:17:24 +0200

    Initial Commit

---
+++ CustomNavigation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomNavigation
+//
+//  Created by Jonni Akesson on 2022-05-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomNavigation/CustomNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomNavigationApp.swift
+//  CustomNavigation
+//
+//  Created by Jonni Akesson on 2022-05-15.
+//
+
+import SwiftUI
+
+@main
+struct CustomNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
